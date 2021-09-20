de8a4d015246dfa119715960355b49c5865b8e9a
Mshary <m.alsharekh94@gmail.com>
2021-09-20 15:19:30 +0300

    Initial commit

---
+++ API/user/controllers.js
@@ -0,0 +1,44 @@
+/* Imports */
+require("dotenv").config();
+const JWT_SECRET = process.env.JWT_SECRET;
+const JWT_EXPIRATION_MS = process.env.JWT_EXPIRATION_MS;
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+/* Models */
+const { User } = require("../../db/models");
+
+/* Controllers */
+exports.register = async (req, res, next) => {
+  const { password } = req.body;
+  const saltRounds = 10;
+  try {
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+    req.body.password = hashedPassword;
+    const newUser = await User.create(req.body);
+    const payload = {
+      id: newUser.id,
+      username: newUser.username,
+      exp: Date.now() + parseInt(JWT_EXPIRATION_MS),
+    };
+    const token = jwt.sign(JSON.stringify(payload), JWT_SECRET);
+    res.json({ token });
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.login = async (req, res, next) => {
+  try {
+    const { user } = req;
+    const payload = {
+      id: user.id,
+      username: user.username,
+      exp: Date.now() + parseInt(JWT_EXPIRATION_MS),
+    };
+    const token = jwt.sign(JSON.stringify(payload), JWT_SECRET);
+    res.json({ token });
+  } catch (error) {
+    next(error);
+  }
+};
